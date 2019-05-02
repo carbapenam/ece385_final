30dea4c4b279fc063c25b16165f229a79a1cb9d7
carbapenam <3783158+carbapenam@users.noreply.github.com>
2019-05-01 21:58:46 -0500

    almost there.

--- util/convert2.py
+++ util/convert2.py
@@ -2,42 +2,94 @@ from PIL import Image
 from bitstring import BitArray
 import sys
 
-# Image format: RGBA, big endian
-class bg():
-    def __init__(self, width, height, addr):
+# Image format: RGBA, little endian
+class Generic_Image():
+    def __init__(self, width, height, addr, image):
         self.width = width
         self.height = height
         self.addr = addr
+        self.image = image
 
-class chara():
-    def __init__(self, width, height, offset_x, offset_y, addr):
+class Chara():
+    def __init__(self, width, height, offset_x, offset_y, addr, image):
         self.width = width
         self.height = height
         self.offset_x = offset_x
         self.offset_y = offset_y
         self.addr = addr
-        
-class font():
-    def __init__(self, width, height, addr):
-        self.width = width
-        self.height = height
-        self.addr = addr
+        self.image = image
 
-class text():
-    def __init__(self, length, addr):
+class Text():
+    def __init__(self, length, addr, text):
         self.length = length
         self.addr = addr
-
-def convert():
-    
-
+        self.text = text
         
-def write_image(file_name):
+def convert(batch, output):
+    background = []
+    chara = []
+    font = []
+    text = []
+    
+    flag = '' #B, C, F, T 
+    for line in batch:
+        if line == "[Background]":
+            flag = 'B'
+            continue
+        if line == "[Character]":
+            flag = 'C'
+            continue
+        if line == "[Font]":
+            flag = 'F'
+            continue
+        if line == "[Script]":
+            flag = 'S'
+            continue
+            
+        if flag == 'B':
+            convert_generic_image(line, background)
+            continue
+        
+        if flag == 'F':
+            convert_generic_image(line, font)
+            continue
+            
+        if flag == 'C':
+            #parse the offset_info
+            convert_chara(line, chara)
+            continue
+            
+        if flag == 'S':
+            #parse quotation
+            convert_text(line, text)
+            continue
+    
+    assemble(background, chara, font, text, output)
+    
+def assemble(background, chara, font, text, file_handle):
+    offset = 0
+    
+    #start with background
+    for bg in background:
+        
+        offset = offset + width*height
+    
+    #chara
+    
+    #font
+    
+    #text
+    
+    
+    
+    #where the file is made
+    
+def return_generic_image(file_name):
     im = Image.open(file_name)
     
-    buf = bytearray()
     width = im.size[0]
     height = im.size[1]
+    buf = b''
     
     for y in range(0, height):
         for x in range(0, width):
@@ -56,10 +108,25 @@ def write_image(file_name):
             + (int(pixel_value[3]) << 8)
             )
             
-    return BitArray(uintle = temp, length=32).bytes
+            buf = buf + BitArray(uintle = temp, length=32).bytes
+    return Generic_Image(width, height, 0, buf)
 
-def write_text
-             
+def convert_generic_image(file_name, image_list):
+    image_list.append(return_generic_image(file_name)) 
+    
+def convert_chara(file_name, offset_x, offset_y, chara_list):
+    temp = return_generic_image(file_name)
+    chara_list.append(temp.width, temp.height, offset_x, offset_y, 0, temp.image)
+    
+def convert_text(line, text_list)
+    buf = line.encode() + b'\x00'
+    zeropad_len = math.ceil(len(buf) / 4.0) - len(buf) 
+    padded_len = math.ceil(len(buf) / 4.0)
+    
+    #zero pad the strings
+    buf = buf + b'\x00'*zeropad_len
+    text_list.append(Text(padded_len, 0, buf))
+    
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         batch = open(sys.argv[1], "r")
